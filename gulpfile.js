15ac5a048db0deacf25c86de1937d91130686b2f
wufei3623 <1737509789@qq.com>
2019-09-16 17:49:37 +0800

    wufei

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 var px2rem = require('gulp-px2rem-plugin')
 // 引入浏览器刷新插件
-let browserSync = require('browser-sync').create();
-
+var browserSync = require('browser-sync').create();
+var proxy = require('http-proxy-middleware')
 var less = require('gulp-less')
 
 /* px 转化为 rem */
@@ -24,11 +24,14 @@ gulp.task('default', function () {
   .pipe(gulp.dest('dist/js'))
 });
 
-
+gulp.task('copyJs', function(){    
+  return gulp.src('src/js/testEval.js')        
+    .pipe(gulp.dest('E:/代码仓库/united_h5/js')) // 将源文件拷贝到打包目录
+});
 //CSS生成文件hash编码并生成 rev-manifest.json文件名对照映射
-gulp.task('copyHtml', function(){    
+gulp.task('copyHtml', ['copyJs'], function(){    
   return gulp.src('src/html/*.html')        
-    .pipe(gulp.dest('F:/代码仓库/united_h5')) // 将源文件拷贝到打包目录
+    .pipe(gulp.dest('E:/代码仓库/united_h5')) // 将源文件拷贝到打包目录
 });
 
 // 编译less
@@ -38,17 +41,26 @@ gulp.task('less', function () {
   // 2. 编译为css
       .pipe(less())
   // 3. 将css中px转化为rem 
-      .pipe(px2rem({'width_design':720,'valid_num':4,'pieces':10}))
+      .pipe(px2rem({'width_design':750,'valid_num':4,'pieces':10,'ignore_px':[1,2]}))
   // 4. 另存文件
-      .pipe(gulp.dest('F:/代码仓库/united_h5/css'))
+      .pipe(gulp.dest('E:/代码仓库/united_h5/css'))
 });
 
 gulp.task('server', function() {
+  var jsonPlaceholderProxy = proxy('/api',{
+    target: 'https://test.iconntech.com',
+    changeOrigin: true,
+    pathRewrite: {
+     '^/api': ''
+      }
+  })
   browserSync.init({
       server: {
-          baseDir: 'F:/代码仓库/united_h5' // 指定项目文件夹
+          baseDir: 'E:/代码仓库/united_h5', // 指定项目文件夹
+          middleware: [jsonPlaceholderProxy]
       }
   })
   gulp.watch('./src/**/*.*',['less','copyHtml']).on('change', browserSync.reload);
+  // gulp.watch('E:/代码仓库/united_h5/**/*.*').on('change', browserSync.reload);
 });
 
---
+++ src/js/testEval.js
@@ -0,0 +1,117 @@
+$(function(){
+  $('.close').on('click',closeEval)
+  toggleName()
+  evalStar()
+  evalDescribe()
+  $('#submit').on('click',submit)
+})
+function closeEval(){
+  history.go(-1)
+}
+function toggleName(){
+  var anonymityImge = ['./images/circle_noCheck.png', './images/cirlce_check.png']
+  $('.right').toggle(function(){
+    $(this).attr('data-checked', '0')
+    $('#submit').text('匿名提交')
+    $('.right img').attr('src', anonymityImge[1])
+  },function(){
+    $(this).attr('data-checked', '1')
+    $('#submit').text('提交')
+    $('.right img').attr('src', anonymityImge[0])
+  })
+}
+function evalStar(){
+  var starImg = ['./images/star_noChecked.png', './images/start_checked.png']
+  var starMsg = ['非常不满意','不满意','满意','比较满意','非常满意']
+  var editableTag = {
+    dissatisfaction:['信息不准确','页面打开慢','办理效率低','流程较复杂'],
+    satisfaction:['信息较准确','页面较流畅','办理较高效','流程较简洁'],
+    quiteSatisfied:['信息准确','页面流畅','办理高效','安全可靠'],
+  }
+  $('.starEval img').on('click',function(event){
+    var imgNum = Number($(this).attr('data-num')) // 点击对象对应的data-num属性
+    console.log(imgNum);
+    
+    $('.textEval').text(starMsg[imgNum])
+    $('.textEval').addClass('startChecked')
+    $('.starEval').attr('data-star',imgNum + 1) // 用户评价的星星数量
+    $('.starEval img').attr('src',starImg[0])
+    for(var i = 0; i <= imgNum; i++){
+      $($('.starEval img')[i]).attr('src', starImg[1])
+    }
+    if(imgNum < 2){
+      // $('.evalDescribe span')
+      editableTag.dissatisfaction.forEach(function(item,index){
+        $($('.evalDescribe span')[index]).text(item)
+      })
+    }else if(imgNum === 2){
+      // $('.evalDescribe span')
+      editableTag.satisfaction.forEach(function(item,index){
+        $($('.evalDescribe span')[index]).text(item)
+      })
+      $('#textarea').attr('placeholder', '请输入您的意见或建议')
+    }else{
+      editableTag.quiteSatisfied.forEach(function(item,index){
+        $($('.evalDescribe span')[index]).text(item)
+      })
+      $('#textarea').attr('placeholder', '请输入您的意见或建议')
+    }
+  })
+}
+function evalDescribe(){
+  for(var i=0;i<4;i++){
+    $($('.evalDescribe span')[i]).toggle(function(){
+      $(this).addClass('checked')
+      // $('.evalDescribe').attr('data-tag', $(this).text())
+    },function(){
+      $(this).removeClass('checked')
+      // $('.evalDescribe').attr('data-tag', $(this).text())
+    })
+  }
+}
+function submit(){
+  var anonymity = $('.right').attr('data-checked') // 是否匿名
+  var star = $('.starEval').attr('data-star') // 星级
+  // var evalDescribe =  $('.evalDescribe').attr('data-tag') // 评价标签
+  var evalDescribe = [];
+  var textareaVal =  $('#textarea').val() // 评价内容
+  // $.ajax()
+  for(var i = 0; i < $('.evalDescribe .checked').length; i++){
+    evalDescribe.push($($('.evalDescribe .checked')[i]).text())
+  }
+  // $.myToast(textareaVal);
+  var url = '/api/h5gateway/evaluate/addEvaluate'
+  var params = {
+    "caseId": getQueryVariable('caseId'),
+    "content": textareaVal,
+    "starLevel": star,
+    "editableTag": JSON.stringify(evalDescribe),
+    "anonymous": anonymity
+  }
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: JSON.stringify(params),
+    xhrFields: {
+      withCredentials: true // 设置运行跨域操作
+    },
+    dataType:'json',
+    contentType: "application/json;charset=UTF-8",
+    success:function(res){
+      $.myToast(res.value);
+    },
+    error: function(e){
+      $.myToast("出错了");
+    }
+  })
+}
+
+function getQueryVariable(variable){
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+          var pair = vars[i].split("=");
+          if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
